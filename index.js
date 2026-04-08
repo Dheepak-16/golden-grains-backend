fe8b06738a1d78dbf0f660e0f5ca8746a8feebdc
tamil-12 <tamilselvan92485@gmail.com>
2026-04-08 22:09:31 +0530

    Added root route

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ server.use(cors());
 server.use(bodyParser.json());
 server.use(express.json());
 server.use(express.urlencoded({extended: false}));
+
+// ✅ ADD THIS
+server.get("/", (req, res) => {
+  res.send("API Running ✅");
+});
+
 server.use('/' , require('./app'));
 server.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);    
